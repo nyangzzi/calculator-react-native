3e799081fcb3708e8b98320da3ca298bec25a51c
eunkyung <ekyi0421@gmail.com>
2024-09-04 16:48:05 +0900

    feat: set Button Container

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
 import { Button, ButtonTypes } from './components/Button';
 import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   const [result, setResult] = useState(0);
 
+  const { width } = useWindowDimensions();
+
   return (
     <View style={styles.container}>
+      <StatusBar style="light" />
+
       <View style={styles.resultContainer}>
         {/* 결과 */}
         <Text style={styles.resultText}>{result}</Text>
       </View>
-      <View style={styles.buttonContainer}>{/* 버튼 */}</View>
+      <View style={styles.buttonContainer}>
+        {/* 버튼 */}
+        <view style={styles.leftPad}>
+          <view style={styles.number}></view>
+          <view style={styles.bottom}></view>
+        </view>
+
+        <view style={styles.operator}></view>
+      </View>
     </View>
   );
 }
@@ -32,6 +45,7 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
+    flexDirection: 'row',
   },
   resultText: {
     fontSize: 60,
@@ -39,4 +53,10 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     padding: 30,
   },
+  leftPad: {},
+  number: {},
+  bottom: {
+    flexDirection: 'row',
+  },
+  operator: {},
 });
