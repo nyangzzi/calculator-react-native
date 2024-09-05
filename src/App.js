0fd626c0f9ed049987a4f0763d643f7736b7c181
eunkyung <ekyi0421@gmail.com>
2024-09-05 13:56:05 +0900

    feat: Add calc function

--- src/App.js
+++ src/App.js
@@ -12,8 +12,51 @@ const Operators = {
 
 export default function App() {
   const [result, setResult] = useState(0);
+  const [formula, setFormula] = useState([]);
 
   const { width } = useWindowDimensions();
+
+  const formatComma = (value) => {
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  };
+
+  const onPressNum = (num) => {
+    if (formula.at(-1) === Operators.EQUAL) {
+      setFormula([]);
+      setResult(num);
+    } else setResult((prev) => prev * 10 + num);
+  };
+
+  const calcNum = () => {
+    let calcResult = 0;
+    [...formula, result].forEach((value, index) => {
+      if (!isNaN(value)) {
+        if (index === 0 || formula[index - 1] === Operators.PLUS)
+          calcResult += value;
+        else calcResult -= value;
+      }
+    });
+    return calcResult;
+  };
+
+  const onPressOperator = (operator) => {
+    switch (operator) {
+      case Operators.CLEAR:
+        setFormula([]);
+        setResult(0);
+        break;
+      case Operators.EQUAL:
+        {
+          setFormula([...formula, result, operator]);
+          setResult(calcNum());
+        }
+        break;
+      default:
+        setFormula([...formula, result, operator]);
+        setResult(0);
+        break;
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -21,7 +64,10 @@ export default function App() {
 
       <View style={[styles.resultContainer, { flex: 1 }]}>
         {/* 결과 */}
-        <Text style={styles.resultText}>{result}</Text>
+        <Text style={styles.formulaText}>
+          {formula.map((value) => formatComma(value))}
+        </Text>
+        <Text style={styles.resultText}>{formatComma(result)}</Text>
       </View>
       <View style={[styles.buttonContainer, { height: width }]}>
         {/* 버튼 */}
@@ -30,7 +76,7 @@ export default function App() {
             {Array.from({ length: 9 }).map((_, index) => (
               <Button
                 title={index + 1}
-                onPress={() => {}}
+                onPress={() => onPressNum(index + 1)}
                 buttonStyle={{ width: '33.1%', height: '33.1%' }}
               />
             ))}
@@ -39,7 +85,9 @@ export default function App() {
             <Button title="0" onPress={() => {}} buttonStyle={{ flex: 2 }} />
             <Button
               title={Operators.EQUAL}
-              onPress={() => {}}
+              onPress={() => {
+                onPressOperator(Operators.EQUAL);
+              }}
               buttonType={ButtonTypes.OPERATOR}
               buttonStyle={{ flex: 1 }}
             />
@@ -49,19 +97,25 @@ export default function App() {
         <View style={styles.operator}>
           <Button
             title={Operators.CLEAR}
-            onPress={() => {}}
+            onPress={() => {
+              onPressOperator(Operators.CLEAR);
+            }}
             buttonType={ButtonTypes.OPERATOR}
             buttonStyle={{ flex: 1 }}
           />
           <Button
             title={Operators.MINUS}
-            onPress={() => {}}
+            onPress={() => {
+              onPressOperator(Operators.MINUS);
+            }}
             buttonType={ButtonTypes.OPERATOR}
             buttonStyle={{ flex: 1 }}
           />
           <Button
             title={Operators.PLUS}
-            onPress={() => {}}
+            onPress={() => {
+              onPressOperator(Operators.PLUS);
+            }}
             buttonType={ButtonTypes.OPERATOR}
             buttonStyle={{ flex: 2 }}
           />
@@ -90,10 +144,19 @@ const styles = StyleSheet.create({
     gap: 1,
     backgroundColor: '#000000',
   },
+  formulaText: {
+    fontSize: 30,
+    color: 'gray',
+    paddingEnd: 20,
+    paddingStart: 20,
+    paddingTop: 20,
+  },
   resultText: {
     fontSize: 60,
     color: '#ffffff',
-    padding: 20,
+    paddingEnd: 20,
+    paddingStart: 20,
+    paddingBottom: 20,
   },
   leftPad: { flex: 3, gap: 1 },
   number: {
